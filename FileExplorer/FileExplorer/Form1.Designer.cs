841f7891884fc50df048357697364daf7386116e
Hiva <hivaabolhadi@gmail.com>
2023-06-25 15:41:10 +0330

    last

--- FileExplorer/FileExplorer/Form1.Designer.cs
+++ FileExplorer/FileExplorer/Form1.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.txtAddFile = new System.Windows.Forms.TextBox();
             this.btnAddFile = new System.Windows.Forms.Button();
             this.btnAddFolder = new System.Windows.Forms.Button();
             this.txtAddFolder = new System.Windows.Forms.TextBox();
             this.btnDelete = new System.Windows.Forms.Button();
-            this.btnCopy = new System.Windows.Forms.Button();
             this.btnPaste = new System.Windows.Forms.Button();
-            this.btnCut = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(308, 25);
             this.dataGridView1.Name = "dataGridView1";
@@ -51,85 +56,105 @@
             this.dataGridView1.RowTemplate.Height = 29;
             this.dataGridView1.Size = new System.Drawing.Size(484, 523);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
             // 
             // txtAddFile
             // 
-            this.txtAddFile.Location = new System.Drawing.Point(48, 46);
+            this.txtAddFile.Location = new System.Drawing.Point(55, 129);
             this.txtAddFile.Name = "txtAddFile";
             this.txtAddFile.Size = new System.Drawing.Size(208, 27);
             this.txtAddFile.TabIndex = 1;
             // 
             // btnAddFile
             // 
-            this.btnAddFile.Location = new System.Drawing.Point(162, 79);
+            this.btnAddFile.Location = new System.Drawing.Point(169, 162);
             this.btnAddFile.Name = "btnAddFile";
             this.btnAddFile.Size = new System.Drawing.Size(94, 29);
             this.btnAddFile.TabIndex = 2;
             this.btnAddFile.Text = "Add File";
             this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
             // 
             // btnAddFolder
             // 
-            this.btnAddFolder.Location = new System.Drawing.Point(162, 158);
+            this.btnAddFolder.Location = new System.Drawing.Point(169, 241);
             this.btnAddFolder.Name = "btnAddFolder";
             this.btnAddFolder.Size = new System.Drawing.Size(94, 29);
             this.btnAddFolder.TabIndex = 4;
             this.btnAddFolder.Text = "Add Folder";
             this.btnAddFolder.UseVisualStyleBackColor = true;
+            this.btnAddFolder.Click += new System.EventHandler(this.btnAddFolder_Click);
             // 
             // txtAddFolder
             // 
-            this.txtAddFolder.Location = new System.Drawing.Point(48, 125);
+            this.txtAddFolder.Location = new System.Drawing.Point(55, 208);
             this.txtAddFolder.Name = "txtAddFolder";
             this.txtAddFolder.Size = new System.Drawing.Size(208, 27);
             this.txtAddFolder.TabIndex = 3;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(162, 216);
+            this.btnDelete.Location = new System.Drawing.Point(169, 299);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(94, 29);
             this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
-            // 
-            // btnCopy
-            // 
-            this.btnCopy.Location = new System.Drawing.Point(162, 269);
-            this.btnCopy.Name = "btnCopy";
-            this.btnCopy.Size = new System.Drawing.Size(94, 29);
-            this.btnCopy.TabIndex = 6;
-            this.btnCopy.Text = "Copy";
-            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnPaste
             // 
-            this.btnPaste.Location = new System.Drawing.Point(162, 391);
+            this.btnPaste.Location = new System.Drawing.Point(169, 361);
             this.btnPaste.Name = "btnPaste";
             this.btnPaste.Size = new System.Drawing.Size(94, 29);
             this.btnPaste.TabIndex = 7;
             this.btnPaste.Text = "Paste";
             this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
             // 
-            // btnCut
+            // btnBack
             // 
-            this.btnCut.Location = new System.Drawing.Point(162, 332);
-            this.btnCut.Name = "btnCut";
-            this.btnCut.Size = new System.Drawing.Size(94, 29);
-            this.btnCut.TabIndex = 8;
-            this.btnCut.Text = "Cut";
-            this.btnCut.UseVisualStyleBackColor = true;
+            this.btnBack.Location = new System.Drawing.Point(169, 67);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(94, 29);
+            this.btnBack.TabIndex = 9;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.cutToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(111, 52);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.copyToolStripMenuItem.Text = "copy";
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.cutToolStripMenuItem.Text = "cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1128, 569);
-            this.Controls.Add(this.btnCut);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnPaste);
-            this.Controls.Add(this.btnCopy);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAddFolder);
             this.Controls.Add(this.txtAddFolder);
@@ -143,6 +168,7 @@
             this.Text = "File Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,8 +182,10 @@
         private Button btnAddFolder;
         private TextBox txtAddFolder;
         private Button btnDelete;
-        private Button btnCopy;
         private Button btnPaste;
-        private Button btnCut;
+        private Button btnBack;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
     }
 }
--- FileExplorer/FileExplorer/Form1.cs
+++ FileExplorer/FileExplorer/Form1.cs
@@ -13,9 +13,17 @@ namespace FileExplorer
     public partial class mainForm : Form
     {
         private string connectionString = "Data Source=fileExplorer.db;Version=3;";
+        private List<int> parentIdHistory = new List<int>();
+        private int selectedId; // متغیری برای ذخیره آیدی انتخاب شده
+        private int parentId;
 
         private void LoadNamesWithParentId(int parentId)
         {
+            // بررسی تاریخچه parentId و در صورت خالی بودن، parentId صفر را اضافه کنید
+            if (parentIdHistory.Count == 0)
+            {
+                parentIdHistory.Add(0);
+            }
             // ایجاد اتصال به پایگاه داده SQLite
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
@@ -48,7 +56,7 @@ namespace FileExplorer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+
             // بارگزاری اسامی با parentId خالی در ابتدا
             LoadNamesWithParentId(0);
             dataGridView1.Columns["Id"].Visible = false;
@@ -62,9 +70,9 @@ namespace FileExplorer
             if (e.RowIndex >= 0)
             {
                 // استخراج آیدی از شناسه سطر کلیک شده
-                int selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+                selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
 
-                MessageBox.Show("The row index = " + selectedId.ToString());
+                //MessageBox.Show("The row index = " + selectedId.ToString());
                 // نمایش اسامی مرتبط با آیدی استخراج شده
                 //LoadNamesWithParentId(selectedId);
             }
@@ -74,14 +82,305 @@ namespace FileExplorer
         {
             if (e.RowIndex >= 0)
             {
-                // استخراج آیدی از شناسه سطر کلیک شده
-                int selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+                int isDirectory = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[2].Value);
+                if (isDirectory == 1)
+                {
+                    // استخراج آیدی از شناسه سطر کلیک شده
+                    selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
 
-                //MessageBox.Show("The row index = " + selectedId.ToString());
-                // نمایش اسامی مرتبط با آیدی استخراج شده
-                LoadNamesWithParentId(selectedId);
+                    //MessageBox.Show("The row index = " + selectedId.ToString());
+                    // اضافه کردن آیدی به تاریخچه parentId
+                    parentIdHistory.Add(selectedId);
+                    // نمایش اسامی مرتبط با آیدی استخراج شده
+                    LoadNamesWithParentId(selectedId);
+                }
             }
 
+        }
+
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // بررسی وجود مقادیر در تاریخچه parentId
+            if (parentIdHistory.Count > 1)
+            {
+                // حذف آخرین parentId از تاریخچه
+                parentIdHistory.RemoveAt(parentIdHistory.Count - 1);
+
+                // بازیابی آخرین parentId
+                int lastParentId = parentIdHistory[parentIdHistory.Count - 1];
+
+                // نمایش اسامی مرتبط با آخرین parentId
+                LoadNamesWithParentId(lastParentId);
+            }
+        }
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            string fileName = txtAddFile.Text.Trim();
+
+            // بررسی وجود اسم خالی
+            if (string.IsNullOrEmpty(fileName))
+            {
+                MessageBox.Show("Please enter the file name.");
+                return;
+            }
+
+            // بررسی تکراری بودن اسم فایل
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                string existingFileName = row.Cells["Name"].Value.ToString();
+                if (fileName == existingFileName)
+                {
+                    MessageBox.Show("The file name is duplicate.");
+                    return;
+                }
+            }
+
+            // گرفتن parentId از جدول
+            parentId = parentIdHistory[parentIdHistory.Count - 1];
+
+            // ایجاد اتصال به پایگاه داده SQLite
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // اجرای کوئری برای افزودن رکورد جدید
+                string query = "INSERT INTO Files (Name, IsDirectory, ParentId) VALUES (@Name, 0, @ParentId)";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", fileName);
+                command.Parameters.AddWithValue("@ParentId", parentId);
+                command.ExecuteNonQuery();
+
+                // بستن اتصال
+                connection.Close();
+                txtAddFile.Text = string.Empty;
+            }
+
+            // بارگزاری اسامی با parentId فعلی برای به‌روزرسانی DataGridView
+            LoadNamesWithParentId(parentId);
+        }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            string folderName = txtAddFolder.Text.Trim();
+
+            // بررسی وجود اسم خالی
+            if (string.IsNullOrEmpty(folderName))
+            {
+                MessageBox.Show("Please enter the folder name.");
+                return;
+            }
+
+            // بررسی تکراری بودن اسم فایل
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                string existingFileName = row.Cells["Name"].Value.ToString();
+                if (folderName == existingFileName)
+                {
+                    MessageBox.Show("The folder name is duplicate.");
+                    return;
+                }
+            }
+
+            // گرفتن parentId از جدول
+            int parentId = parentIdHistory[parentIdHistory.Count - 1];
+
+            // ایجاد اتصال به پایگاه داده SQLite
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // اجرای کوئری برای افزودن رکورد جدید
+                string query = "INSERT INTO Files (Name, IsDirectory, ParentId) VALUES (@Name, 1, @ParentId)";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", folderName);
+                command.Parameters.AddWithValue("@ParentId", parentId);
+                command.ExecuteNonQuery();
+
+                // بستن اتصال
+                connection.Close();
+                txtAddFolder.Text = string.Empty;
+            }
+
+            // بارگزاری اسامی با parentId فعلی برای به‌روزرسانی DataGridView
+            LoadNamesWithParentId(parentId);
+        }
+        
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+                selectedName = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                parentId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+            }
+        }
+        private void DeleteRecord(int id)
+        {
+            // حذف رکورد با آیدی مشخص شده
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string deleteQuery = "DELETE FROM Files WHERE Id = @Id";
+                SQLiteCommand command = new SQLiteCommand(deleteQuery, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string deleteQuery = "DELETE FROM Files WHERE parentId = @Id";
+                SQLiteCommand command = new SQLiteCommand(deleteQuery, connection);
+                command.Parameters.AddWithValue("@Id", id);
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedId != 0)
+            {
+                string selectedName = dataGridView1.Rows.Cast<DataGridViewRow>()
+                    .Where(row => Convert.ToInt32(row.Cells["Id"].Value) == selectedId)
+                    .Select(row => row.Cells["Name"].Value.ToString())
+                    .FirstOrDefault();
+
+                // نمایش پیغام هشدار و دریافت پاسخ کاربر
+                DialogResult result = MessageBox.Show($"Are you sure you want to delete the '{selectedName}'?",
+                    "Warning: Deletion Alert.", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    // حذف رکورد با آیدی مشخص شده
+                    DeleteRecord(selectedId);
+
+                    // بروزرسانی جدول DataGridView
+                    LoadNamesWithParentId(parentId);
+                }
+            }
+
+
+        }
+        
+        private void UpdateParentId(int id, int newParentId)
+        {
+            // ایجاد اتصال به پایگاه داده SQLite و بروزرسانی parentId رکورد
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE Files SET parentId = @NewParentId WHERE Id = @Id";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@NewParentId", newParentId);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+        private bool CheckDuplicateName(string name)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[1].Value != null && row.Cells[1].Value.ToString() == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool CheckDuplicateInTable(string name, int parentId)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[1].Value != null && row.Cells[1].Value.ToString() == name)
+                {
+                    int rowParentId = Convert.ToInt32(row.Cells[3].Value);
+                    if (rowParentId == parentId)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            if (isCutMode)
+            {
+                int lastParentId = parentIdHistory[parentIdHistory.Count - 1];
+                int destinationParentId = lastParentId;
+
+                // شرط اول: بررسی تکراری بودن اسم قبل از انجام عملیات پیست
+                bool isDuplicateName = CheckDuplicateName(selectedName);
+                if (isDuplicateName)
+                {
+                    MessageBox.Show("نام تکراری وجود دارد. پیست امکان‌پذیر نیست.");
+                    return;
+                }
+
+                // شرط دوم: بررسی تکراری بودن اسم در جدول
+                bool isDuplicateInTable = CheckDuplicateInTable(selectedName, destinationParentId);
+                if (isDuplicateInTable)
+                {
+                    MessageBox.Show("نام تکراری وجود دارد. پیست امکان‌پذیر نیست.");
+                    return;
+                }
+
+                // انجام عملیات paste
+                UpdateParentId(selectedIdcut, destinationParentId);
+                isCutMode = false;
+                LoadNamesWithParentId(lastParentId);
+            }
+        }
+
+        private int selectedParentId;
+        private int selectedIdcut;
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+        string selectedName;
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                // اگر روی سلول کلیک راست شد
+                if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+                {
+                    // خواندن Id سلول از DataGridView
+                    selectedName = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                    selectedIdcut = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+                    int parentId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[3].Value);
+
+                    // شرط اول: اگر ParentId برابر با 0 یا 1 باشد، کاربر نمی‌تواند روی سلول کلیک کند
+                    if (parentId == 0 || parentId == 1)
+                    {
+                        return;
+                    }
+
+                    // ذخیره کردن Id در خصوصیت Tag سلول
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Tag = selectedIdcut;
+
+                    // نمایش منوی راست کلیک
+                    contextMenuStrip1.Show(dataGridView1, e.Location);
+                }
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+        }
+        bool isCutMode;
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isCutMode=true;
         }
     }
 }
