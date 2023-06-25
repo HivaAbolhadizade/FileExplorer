b137aab01b5f2f841020193fdda3993ade155997
Hiva <hivaabolhadi@gmail.com>
2023-06-25 08:06:57 +0330

    cell select

--- FileExplorer/FileExplorer/Form1.Designer.cs
+++ FileExplorer/FileExplorer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FileExplorer
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAddFile = new System.Windows.Forms.TextBox();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnAddFolder = new System.Windows.Forms.Button();
+            this.txtAddFolder = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnCut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(308, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(484, 523);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // txtAddFile
+            // 
+            this.txtAddFile.Location = new System.Drawing.Point(48, 46);
+            this.txtAddFile.Name = "txtAddFile";
+            this.txtAddFile.Size = new System.Drawing.Size(208, 27);
+            this.txtAddFile.TabIndex = 1;
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(162, 79);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(94, 29);
+            this.btnAddFile.TabIndex = 2;
+            this.btnAddFile.Text = "Add File";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFolder
+            // 
+            this.btnAddFolder.Location = new System.Drawing.Point(162, 158);
+            this.btnAddFolder.Name = "btnAddFolder";
+            this.btnAddFolder.Size = new System.Drawing.Size(94, 29);
+            this.btnAddFolder.TabIndex = 4;
+            this.btnAddFolder.Text = "Add Folder";
+            this.btnAddFolder.UseVisualStyleBackColor = true;
+            // 
+            // txtAddFolder
+            // 
+            this.txtAddFolder.Location = new System.Drawing.Point(48, 125);
+            this.txtAddFolder.Name = "txtAddFolder";
+            this.txtAddFolder.Size = new System.Drawing.Size(208, 27);
+            this.txtAddFolder.TabIndex = 3;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(162, 216);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 29);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(162, 269);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(94, 29);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(162, 391);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(94, 29);
+            this.btnPaste.TabIndex = 7;
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            // 
+            // btnCut
+            // 
+            this.btnCut.Location = new System.Drawing.Point(162, 332);
+            this.btnCut.Name = "btnCut";
+            this.btnCut.Size = new System.Drawing.Size(94, 29);
+            this.btnCut.TabIndex = 8;
+            this.btnCut.Text = "Cut";
+            this.btnCut.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1128, 569);
+            this.Controls.Add(this.btnCut);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddFolder);
+            this.Controls.Add(this.txtAddFolder);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.txtAddFile);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox txtAddFile;
+        private Button btnAddFile;
+        private Button btnAddFolder;
+        private TextBox txtAddFolder;
+        private Button btnDelete;
+        private Button btnCopy;
+        private Button btnPaste;
+        private Button btnCut;
     }
 }
--- FileExplorer/FileExplorer/Form1.cs
+++ FileExplorer/FileExplorer/Form1.cs
@@ -1,10 +1,87 @@
+﻿using System.Data.SQLite;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
 namespace FileExplorer
 {
-    public partial class Form1 : Form
+    public partial class mainForm : Form
     {
-        public Form1()
+        private string connectionString = "Data Source=fileExplorer.db;Version=3;";
+
+        private void LoadNamesWithParentId(int parentId)
+        {
+            // ایجاد اتصال به پایگاه داده SQLite
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // اجرای کوئری برای بازیابی اسامی با parentId مشخص شده
+                string query = "SELECT * FROM Files WHERE parentId = @ParentId";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                command.Parameters.AddWithValue("@ParentId", parentId);
+
+                // اجرای کوئری و بازیابی نتایج
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                // ایجاد یک DataTable برای ذخیره اسامی
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+
+                // نمایش اسامی در DataGridView
+                dataGridView1.DataSource = dataTable;
+
+                // بستن اتصال و رفع منابع
+                reader.Close();
+                connection.Close();
+            }
+        }
+        public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            // بارگزاری اسامی با parentId خالی در ابتدا
+            LoadNamesWithParentId(0);
+            dataGridView1.Columns["Id"].Visible = false;
+            dataGridView1.Columns["IsDirectory"].Visible = false;
+            dataGridView1.Columns["parentId"].Visible = false;
+
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // استخراج آیدی از شناسه سطر کلیک شده
+                int selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+
+                MessageBox.Show("The row index = " + selectedId.ToString());
+                // نمایش اسامی مرتبط با آیدی استخراج شده
+                //LoadNamesWithParentId(selectedId);
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // استخراج آیدی از شناسه سطر کلیک شده
+                int selectedId = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+
+                //MessageBox.Show("The row index = " + selectedId.ToString());
+                // نمایش اسامی مرتبط با آیدی استخراج شده
+                LoadNamesWithParentId(selectedId);
+            }
+
         }
     }
 }
